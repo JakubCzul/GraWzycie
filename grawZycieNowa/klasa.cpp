24773e56dea4e46b263b8c8da7545a3eee734d22
PioBis778 <pb305347@student.polsl.pl>
2023-12-14 16:28:00 +0100

    com_update

--- grawZycieNowa/klasa.cpp
+++ grawZycieNowa/klasa.cpp
@@ -1,1 +1,86 @@
 #include "klasa.h"
+
+Plansza::Plansza(int szerPlanszy, int wysPlanszy) : m_szerMapy(szerPlanszy), m_wysMapy(wysPlanszy) {
+	tablicaBool.resize(szerPlanszy, std::vector<StanPola>(wysPlanszy, StanPola::Nieaktywne));
+}
+Plansza::~Plansza() {}
+
+void Plansza::rozmiarPlanszy() {
+	std::cout << "Wymiary Planszy\n Wysokosc: " << m_wysMapy << "  Szerokosc: " << m_szerMapy << std::endl;
+}
+
+int Plansza::aktywneSektory() {
+	for (int i = 0; i < m_szerMapy; ++i) {
+		for (int j = 0; j < m_wysMapy; ++j) {
+			if (tablicaBool[i][j] == StanPola::Aktywne) {
+				licznikAktywneSektory++;
+			}
+		}
+	}
+	return licznikAktywneSektory;
+}
+
+void Plansza::zmianaSektoruKlikniecie(int wys, int szer) {
+	if (tablicaBool[wys][szer] == StanPola::Aktywne) {
+		tablicaBool[wys][szer] = StanPola::Nieaktywne;
+	}
+	else
+	{
+		tablicaBool[wys][szer] = StanPola::Aktywne;
+	}
+}
+
+void Plansza::nastepnePokolenie() {
+	std::vector<std::vector<StanPola>> nowaPlansza = tablicaBool;
+
+	for (int i = 0; i < m_szerMapy; ++i) {
+		for (int j = 0; j < m_wysMapy; ++j) {
+			int liczbaAktywnychSasiadow;
+
+			if (tablicaBool[i][j] == StanPola::Aktywne) {
+				nowaPlansza[i][j] = (liczbaAktywnychSasiadow == 3) ? StanPola::Aktywne : StanPola::Nieaktywne;
+			}
+			else {
+				nowaPlansza[i][j] = (liczbaAktywnychSasiadow == 3 || liczbaAktywnychSasiadow == 5) ? StanPola::Aktywne : StanPola::Nieaktywne;
+			}
+		}
+	}
+}
+
+int Plansza::getWysokosc() { return m_wysMapy; }
+
+int Plansza::getSzerokosc() { return m_szerMapy; }
+
+void Plansza::kontrolaCzasuGry(bool PrzyciskCzasu) {
+	const std::chrono::seconds interwal(1);
+	if (PrzyciskCzasu == true) {
+		while (PrzyciskCzasu == false) {
+			nastepnePokolenie();
+			std::this_thread::sleep_for(interwal);
+		}
+	}
+}
+
+void Plansza::zmianaRozmiaruPlanszy(int nowaWys, int nowaSzer) {
+	tablicaBool.resize(nowaSzer, std::vector<StanPola>(nowaWys, StanPola::Nieaktywne));
+}
+
+void Opcje::zmianaRozmiaru(int szer, int wys) {
+	plansza->zmianaRozmiaruPlanszy(szer, wys);
+}
+
+void Opcje::zapis(char nazwaPliku, int ROZMIAR) {
+	//zapis aktualnego stanu gry
+}
+
+void Opcje::wczytaj(const char* nazwaPliku) {
+	//wczytanie zapisanego stanu gry
+}
+
+void OknoInformacyjne::wielkoscPopulacji() {
+	std::cout << "Aktualna populacja: " << 100*(plansza->aktywneSektory());
+}
+
+void OknoInformacyjne::aktualnyRozmiarPlanszy() {
+	std::cout << "Aktualny rozmiar planszy: " << plansza->getWysokosc() << " x " << plansza->getSzerokosc();
+}
--- grawZycieNowa/klasa.h
+++ grawZycieNowa/klasa.h
@@ -8,60 +8,38 @@ private:
 	int m_szerMapy;
 	int m_wysMapy;
 	std::vector<std::vector<StanPola>> tablicaBool;
-	int aktywneSektory = 0;
+	int licznikAktywneSektory = 0;
 public:
-	Plansza(int szerMapy, int wysMapy) : m_szerMapy(szerMapy), m_wysMapy(wysMapy) {
-		tablicaBool.resize(szerMapy, std::vector<StanPola>(wysMapy, StanPola::Nieaktywne));
-	}
-	~Plansza() {}
-	void rozmiarPlanszy() {	
-		std::cout << "Wymiary Planszy\n Wysokosc: " << m_wysMapy << "  Szerokosc: " << m_szerMapy << std::endl;
-	}
+	Plansza(int szerMapy, int wysMapy);
+	~Plansza();
+	void rozmiarPlanszy();
+	int aktywneSektory();
+	void zmianaSektoruKlikniecie(int wys, int szer);
+	void nastepnePokolenie();
+	void kontrolaCzasuGry(bool PrzyciskCzasu);
+	void zmianaRozmiaruPlanszy(int nowaWys, int nowaSzer);
 
-	
+	int getWysokosc();
+	int getSzerokosc();
 
-	void zmianaSektoruKlikniecie(int wys,int szer) {
-		if (tablicaBool[wys][szer] == StanPola::Aktywne) {
-			tablicaBool[wys][szer] = StanPola::Nieaktywne;
-		}
-		else 
-		{
-			tablicaBool[wys][szer] = StanPola::Aktywne;
-		}
-	}
-	void nastepnePokolenie() {
-		std::vector<std::vector<StanPola>> nowaPlansza = tablicaBool;
-
-		for (int i = 0; i < m_szerMapy; ++i) {
-			for (int j = 0; j < m_wysMapy; ++j) {
-				int liczbaAktywnychSasiadow;
-
-				if (tablicaBool[i][j] == StanPola::Aktywne) {
-					// Jeœli s¹ 3 aktywne pola wokó³, pozostaw pole aktywne, w przeciwnym razie dezaktywuj
-					nowaPlansza[i][j] = (liczbaAktywnychSasiadow == 3) ? StanPola::Aktywne : StanPola::Nieaktywne;
-				}
-				else {
-					// Jeœli s¹ 3 lub 5 aktywnych pól wokó³, aktywuj pole
-					nowaPlansza[i][j] = (liczbaAktywnychSasiadow == 3 || liczbaAktywnychSasiadow == 5) ? StanPola::Aktywne : StanPola::Nieaktywne;
-				}
-			}
-		}
-	}
-
-	void kontrolaCzasuGry(bool PrzyciskCzasu) {
-		const std::chrono::seconds interwal(1);
-		if (PrzyciskCzasu == true) {
-			while (PrzyciskCzasu == false) {
-				nastepnePokolenie();
-				std::this_thread::sleep_for(interwal);
-			}
-		}
-	}
-
+	friend Opcje;
 };
 
 
-class OknoInfo {};
-class Opcje {};
+class OknoInformacyjne {
+public:
+	Plansza* plansza;
+	void wielkoscPopulacji();
+	void aktualnyRozmiarPlanszy();
+};
+
+class Opcje {
+public:
+	Plansza* plansza;
+	void zmianaRozmiaru(int szer, int wys);
+	void zapis(char nazwaPliku, int ROZMIAR);
+	void wczytaj(const char* nazwaPliku);
+};
+
 class PrzyciskiRuchu {};
  
