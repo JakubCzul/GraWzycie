c0cbec0ab14bf06375445a6c5750745fc4384d12
JakubCzul <jc305354@student.polsl.pl>
2024-01-20 18:49:56 +0100

    data

--- QtGra/build-grawzycie-Desktop_Qt_6_3_2_MSVC2019_64bit-Debug/ui_mainwindow.h
+++ QtGra/build-grawzycie-Desktop_Qt_6_3_2_MSVC2019_64bit-Debug/ui_mainwindow.h
@@ -10,9 +10,13 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QDateEdit>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,23 +26,45 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QDateEdit *dateActuall;
+    QPushButton *moveButton;
+    QPushButton *saveButton;
+    QPushButton *loadButton;
     QMenuBar *menubar;
+    QMenu *menuGra_w_ycie;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(780, 429);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        dateActuall = new QDateEdit(centralwidget);
+        dateActuall->setObjectName(QString::fromUtf8("dateActuall"));
+        dateActuall->setGeometry(QRect(600, 10, 151, 31));
+        moveButton = new QPushButton(centralwidget);
+        moveButton->setObjectName(QString::fromUtf8("moveButton"));
+        moveButton->setGeometry(QRect(600, 100, 151, 61));
+        saveButton = new QPushButton(centralwidget);
+        saveButton->setObjectName(QString::fromUtf8("saveButton"));
+        saveButton->setGeometry(QRect(600, 270, 151, 31));
+        loadButton = new QPushButton(centralwidget);
+        loadButton->setObjectName(QString::fromUtf8("loadButton"));
+        loadButton->setGeometry(QRect(600, 310, 151, 61));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 780, 21));
+        menuGra_w_ycie = new QMenu(menubar);
+        menuGra_w_ycie->setObjectName(QString::fromUtf8("menuGra_w_ycie"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuGra_w_ycie->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -48,6 +74,10 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        moveButton->setText(QCoreApplication::translate("MainWindow", "MOVE", nullptr));
+        saveButton->setText(QCoreApplication::translate("MainWindow", "SAVE", nullptr));
+        loadButton->setText(QCoreApplication::translate("MainWindow", "LOAD", nullptr));
+        menuGra_w_ycie->setTitle(QCoreApplication::translate("MainWindow", "Gra w \305\273ycie", nullptr));
     } // retranslateUi
 
 };
--- QtGra/grawzycie/mainwindow.cpp
+++ QtGra/grawzycie/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->dateActuall->setDate(QDate::currentDate());
 }
 
 MainWindow::~MainWindow()
